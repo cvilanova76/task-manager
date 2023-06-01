69b8fae6d3dc5135782c7ec28b4cf8e6aaff8296
cvilanova76 <carolinavilanova@gmail.com>
2023-06-01 18:38:59 -0300

    criar e listar tarefas

--- taskmanager/src/main/java/br/com/carolinavn/taskmanager/repository/TaskRepository.java
+++ taskmanager/src/main/java/br/com/carolinavn/taskmanager/repository/TaskRepository.java
@@ -1,5 +1,11 @@
 package br.com.carolinavn.taskmanager.repository;
 
-public interface TaskRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.carolinavn.taskmanager.model.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
     
 }
--- taskmanager/src/main/java/br/com/carolinavn/taskmanager/service/TaskService.java
+++ taskmanager/src/main/java/br/com/carolinavn/taskmanager/service/TaskService.java
@@ -1,5 +1,34 @@
 package br.com.carolinavn.taskmanager.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import br.com.carolinavn.taskmanager.model.Task;
+import br.com.carolinavn.taskmanager.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public Task createTask (Task task){
+        return taskRepository.save(task);
+    }
+
+    public List<Task> listAllTasks(){
+        return taskRepository.findAll();
+    }
+
+    public ResponseEntity<Task> findTaskById(Long id){
+        return taskRepository.findById(id)
+        .map(task -> ResponseEntity.ok().body(task))
+        .orElse(ResponseEntity.notFound().build());
+    }
     
 }
